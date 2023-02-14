1bf8843b6f0e91be0f01dd7d809a40e2989fcc61
João Capucho <jcapucho7@gmail.com>
2023-02-14 21:40:29 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <a href="https://glua.ua.pt/" target="_blank">
+          <img src="/glua-logo.svg" className="logo" alt="Glua logo" />
+        </a>
+      </div>
+      <h1>Github actions workshop</h1>
+      <p className="desc">Build and deploy a react app</p>
+    </div>
+  )
+}
+
+export default App
