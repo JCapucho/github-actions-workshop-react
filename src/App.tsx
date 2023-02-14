e707c12f8896cb50e8b0a5813fc6351cc09070c8
João Capucho <jcapucho7@gmail.com>
2023-02-14 22:14:03 +0000

    Add deploy workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import gluaLogoUrl from "/glua-logo.svg"
 import './App.css'
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
     <div className="App">
       <div>
         <a href="https://glua.ua.pt/" target="_blank">
-          <img src="/glua-logo.svg" className="logo" alt="Glua logo" />
+          <img src={gluaLogoUrl} className="logo" alt="Glua logo" />
         </a>
       </div>
       <h1>Github actions workshop</h1>
